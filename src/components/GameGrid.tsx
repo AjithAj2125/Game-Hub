96fb1db86a0800421cc22a622913787cf434c778
AjithAj2125 <ajithaj2125@gmail.com>
2024-01-06 20:25:55 +0530

    Created custom hooks to fetch games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,34 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import apiClient from '../Services/api-client';
-import { Text } from '@chakra-ui/react';
+import { Text } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
 
-interface Games{
-    id : number;
-    name : string;
-}
-interface FetchGamesResponse{
-    count : number;
-    results : Games[];
-}
 const GameGrid = () => {
-    const [games,setGames] = useState<Games[]>([]);
-    const[error,seterror] = useState("");
+  const { games, error } = useGames();
 
-    useEffect(() => {
-        apiClient.get<FetchGamesResponse>('/games')
-        .then(res => setGames(res.data.results))
-        .catch(err => seterror(err.message));
-
-    })
   return (
     <>
-    {error.length === 0 ? <Text>{error}</Text> : <ul>
-        {games.map(game => <li key={game.id}>{game.name}</li>)}
-    </ul>}
-    
+      <ul>
+        {games.map((game) => (
+          <li key={game.id}>{game.name}</li>
+        ))}
+      </ul>
     </>
-    
-  )
-}
+  );
+};
 
-export default GameGrid
+export default GameGrid;
