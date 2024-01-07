4068016a9c038705628c2fe7179cd6bdbcd4b217
AjithAj2125 <ajithaj2125@gmail.com>
2024-01-07 10:53:22 +0530

    Fetched the genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
-import { Button, ButtonGroup, Grid, GridItem ,Show} from '@chakra-ui/react'
-import NavBar from './components/NavBar'
-import GameGrid from './components/GameGrid'
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
-  return <Grid templateAreas={{base : `"nav main"` ,
-  lg :`"nav nav" "aside main"`}}>
-    <GridItem area='nav'><NavBar ></NavBar></GridItem>
+  return (
+    <Grid templateAreas={{ base: `"nav main"`, lg: `"nav nav" "aside main"` }}>
+      <GridItem area="nav">
+        <NavBar></NavBar>
+      </GridItem>
 
-    <Show above="lg">
-    <GridItem area='aside' >Aside</GridItem>
-    </Show>
-    
-    <GridItem area='main'>
-      <GameGrid/>
-    </GridItem>
+      <Show above="lg">
+        <GridItem area="aside">
+          <GenreList></GenreList>
+        </GridItem>
+      </Show>
+
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
     </Grid>
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/useGenre.ts
@@ -0,0 +1,40 @@
+import { CanceledError } from "axios";
+import { useState, useEffect } from "react";
+import apiClient from "../Services/api-client";
+import { Games } from "./useGames";
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface FetchGenresResponse {
+  count: number;
+  results: Genre[];
+}
+const useGenre = () => {
+  const [genres, setGenre] = useState<Genre[]>([]);
+  const [error, seterror] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    setIsLoading(true);
+    apiClient
+      .get<FetchGenresResponse>("/genres", { signal: controller.signal })
+      .then((res) => {
+        setGenre(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        seterror(err.message);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { genres, error, isLoading };
+};
+
+export default useGenre;
